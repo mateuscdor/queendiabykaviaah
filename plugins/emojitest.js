88f18ad17ac8a42a998c9d1e0d67e7ba3d735797
diabykavi <108246284+diabykavi@users.noreply.github.com>
2022-06-27 11:44:18 +0530

    s

---
+++ plugins/emojitest.js
@@ -0,0 +1,19 @@
+const { EmojiAPI } = require("emoji-api");
+const config = require('../config');
+const prefix = '.'
+async function emoji(conn , mek , q ) {
+  const from = mek.key.remoteJid
+
+
+const emoji = new EmojiAPI();
+emoji.get("🥺")
+    .then(emoji => {
+        console.log(emoji.images[4].url);
+  await conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )
+    })
+
+                          
+
+}
+
+module.exports =  emoji ;
