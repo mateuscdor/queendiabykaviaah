def884b129041711adebd5d9897a54b0341726e5
diabykavi <108246284+diabykavi@users.noreply.github.com>
2022-06-27 13:43:40 +0530

    Update emojitest.js

--- plugins/emojitest.js
+++ plugins/emojitest.js
@@ -16,7 +16,7 @@ emoji.get(q)
   ]
   
   const buttonMessage = {
-      image: {url: '${emoji.images[4].url}'},
+      image: {url: emoji.images[4].url },
       caption: "${q} converted to a image",
       footer: 'QUEEN DIANA EMOJI CONVERTER',
       buttons: buttons,
@@ -24,42 +24,10 @@ emoji.get(q)
   }
   
    conn.sendMessage(from, buttonMessage) 
-  
-    })
-
-// await conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )                         
+                      
 
 }
 
 module.exports =  emoji ;
 
 
-/* 
-const { EmojiAPI } = require("emoji-api");
-const config = require('../config');
-const fs = require('fs');
-const axios = require('axios');
-
-const prefix = '.'
-
-
-async function emoji(conn , mek , q ) {
-  const from = mek.key.remoteJid
-
-
-const emoji = new EmojiAPI();
-emoji.get(q)
-    .then(emoji => {
-       // console.log(emoji.images[4].url);
-  conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )
-
-  var emtestimg = await axios.get(`${emoji.images[4].url}`, { responseType: 'arraybuffer' })
-  conn.sendMessage(from , (emtestimg.data), MessageType.image, { mimetype: Mimetype.jpg})
-  
-    })
-
-                          
-
-}
-
-module.exports =  emoji ;*/
