931bb8f85391386dff91f3b8a1c4bd949efba40c
diabykavi <108246284+diabykavi@users.noreply.github.com>
2022-06-27 13:03:17 +0530

    s

--- plugins/emojitest.js
+++ plugins/emojitest.js
@@ -12,8 +12,39 @@ emoji.get(q)
   conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )
     })
 
-                          
+ await conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )                         
 
 }
 
 module.exports =  emoji ;
+
+
+/* 
+const { EmojiAPI } = require("emoji-api");
+const config = require('../config');
+const fs = require('fs');
+const axios = require('axios');
+
+const prefix = '.'
+
+
+async function emoji(conn , mek , q ) {
+  const from = mek.key.remoteJid
+
+
+const emoji = new EmojiAPI();
+emoji.get(q)
+    .then(emoji => {
+       // console.log(emoji.images[4].url);
+  conn.sendMessage(from , { text: emoji.images[4].url }, { quoted: mek } )
+
+  var emtestimg = await axios.get(`${emoji.images[4].url}`, { responseType: 'arraybuffer' })
+  conn.sendMessage(from , (emtestimg.data), MessageType.image, { mimetype: Mimetype.jpg})
+  
+    })
+
+                          
+
+}
+
+module.exports =  emoji ;*/
