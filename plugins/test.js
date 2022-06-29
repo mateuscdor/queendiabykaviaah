f32da64f78b70f2562579b2676355314be40fb4b
diabykavi <108246284+diabykavi@users.noreply.github.com>
2022-06-29 09:12:33 +0530

    Update test.js

--- plugins/test.js
+++ plugins/test.js
@@ -1,31 +1,17 @@
+
 const events = require("./events");
 
 
 events.cmd({ name : 'hi' , desc : 'nothing'}  , ( async (conn , mek , q) => {
   const from = mek.key.remoteJid
-  const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
-  const senderNumber = sender.split('@')[0]
-			const botNumber = conn.user.id.split(':')[0]
-			const pushname = mek.pushName || 'Sin Nombre'
-			
-			const isMe = botNumber.includes(senderNumber)
-  if (!isMe) return
   await conn.sendMessage(from, { text: 'hi hello' }, { quoted: mek }) 
-} )
+} 
            
-           }
+))
            
            
-           events.cmd({ name : 'hello' , desc : 'nothing'}  , ( async (conn , mek , q) => {
-  const from = mek.key.remoteJid
-  const sender = mek.key.fromMe ? (conn.user.id.split(':')[0]+'@s.whatsapp.net' || conn.user.id) : (mek.key.participant || mek.key.remoteJid)
-  const senderNumber = sender.split('@')[0]
-			const botNumber = conn.user.id.split(':')[0]
-			const pushname = mek.pushName || 'Sin Nombre'
-			
-			const isMe = botNumber.includes(senderNumber)
-  if (!isMe) return
-  await conn.sendMessage(from, { text: 'hi hello 2' }, { quoted: mek }) 
-} )
-           
-           }
+  
+
+
+
+      
