dd635e970af02ae6520efda20edfcdcd1ffd5ebe
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-07-02 09:01:33 +0530

    Create ccty.js

---
+++ plugins/ccty.js
@@ -0,0 +1,21 @@
+
+const lusifarapi = require('textmaker-kaviyaah');
+
+case 'devil':
+
+    if (!q) return await conn.sendMessage(from , { text: "need text" }, { quoted: mek } )
+lusifarapi.textpro("https://textpro.me/create-neon-devil-wings-text-effect-online-free-1014.html",
+        `${q}`
+    ).then(async(data) => {
+        try {
+        
+           
+            await conn.sendMessage(from ,{ image: { url : data }  , caption: config.CAPTION } , { quoted: mek })
+
+
+        } catch (err) {
+                    await conn.sendMessage(from , { text: cantfind}, { quoted: mek } )  
+        }
+    });
+break
+	
