70bdc3dfb9d983d8e54601286347d8732222cc6c
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-07-04 21:28:34 +0530

    Update sss.js

--- plugins/sss.js
+++ plugins/sss.js
@@ -4,18 +4,18 @@ switch (diacomm) {
 case 'diaw':
 const from = mek.key.remoteJid
 
-    if (!q) return await conn.sendMessage(from , { text: "need text" }, { quoted: mek } )
+    if (!q) return conn.sendMessage(from , { text: "need text" }, { quoted: mek } )
 lusifarapi.textpro("https://textpro.me/create-neon-devil-wings-text-effect-online-free-1014.html",
         `${q}`
     ).then(async(data) => {
         try {
         
            
-            await conn.sendMessage(from ,{ image: { url : data }  , caption: config.CAPTION } , { quoted: mek })
+             conn.sendMessage(from ,{ image: { url : data }  , caption: config.CAPTION } , { quoted: mek })
 
 
         } catch (err) {
-                    await conn.sendMessage(from , { text: cantfind}, { quoted: mek } )  
+                   conn.sendMessage(from , { text: cantfind}, { quoted: mek } )  
         }
     });
 break
