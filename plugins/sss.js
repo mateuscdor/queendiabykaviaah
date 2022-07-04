b115cf1a2086390338afce262de741d36a00a3b9
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-07-04 21:22:48 +0530

    J

---
+++ plugins/sss.js
@@ -0,0 +1,23 @@
+const diacomm = commdia
+const lusifarapi = require('textmaker-kaviyaah');
+switch (diacomm) {
+case 'diaw':
+const from = mek.key.remoteJid
+
+    if (!q) return await conn.sendMessage(from , { text: "need text" }, { quoted: mek } )
+lusifarapi.textpro("https://textpro.me/create-neon-devil-wings-text-effect-online-free-1014.html",
+        `${q}`
+    ).then(async(data) => {
+        try {
+        
+           
+            await conn.sendMessage(from ,{ image: { url : data }  , caption: config.CAPTION } , { quoted: mek })
+
+
+        } catch (err) {
+                    await conn.sendMessage(from , { text: cantfind}, { quoted: mek } )  
+        }
+    });
+break
+	
+}
