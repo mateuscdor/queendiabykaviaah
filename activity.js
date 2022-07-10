56646a663c86534826698e6f28f026afc3417202
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-07-10 09:10:32 +0530

    km

---
+++ activity.js
@@ -0,0 +1,7 @@
+function b64utf8(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        return decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+    }
+ module.exports = b64utf8
