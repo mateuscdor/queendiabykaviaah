61463dd4a6339032a50848e20d9261e210c09c7e
kavishka <91202334+kavishkaya@users.noreply.github.com>
2022-07-13 21:15:38 +0530

    J

--- verify.js
+++ verify.js
@@ -1,10 +1,10 @@
 const exec = require('child_process').exec;
 const os = require("os");
 async function startdiana() {
-  var sdn = 'RUN git  https://github.com/diabykavii/queendiabykaaviaah /root/queendiana' + '\n'
-  exec('sed -n 3p /root/queendiana/queendiabykaaviaah/Dockerfile', async (err, stdout, stderr) => {
+  var sdn = 'RUN git  https://github.com/diabykavi/queendiabykaviaah /root/queendiana' + '\n'
+  exec('sed -n 3p /root/queendiana/queendiabykaviaah/Dockerfile', async (err, stdout, stderr) => {
     if (sdn !== stdout) {
-      console.log("😒 Fake User Found •• Only For QUEEN DIANA-Bot Users !!");
+      throw new Error("😒 Fake User Found •• Only For QUEEN DIANA-Bot Users !!");
     }
   })
 }
